0505264cbe50268f893684c94d3208d721d661af
AleksandrSotnikov <sotnikovalex143@gmail.com>
2021-11-01 11:19:56 +0300

    created

---
+++ app/src/main/java/ru/sotnikov/weather/MainActivity.kt
@@ -0,0 +1,37 @@
+package ru.sotnikov.weather
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ru.sotnikov.weather.presentation.ui.theme.WeatherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherTheme {
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WeatherTheme {
+        Greeting("Android")
+    }
+}
